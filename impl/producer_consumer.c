da1f4f06a425fb161282775bae273292bde234d0
fsdvn1 <fsdvn1@gmail.com>
2025-07-14 11:58:51 +0000

    Fix compile errors.
    This reverts commit 2924db471350752ce23e89bc1ed6a0a5919f8956.

--- impl/producer_consumer.c
+++ impl/producer_consumer.c
@@ -9,15 +9,8 @@
 #include <string.h>
 #include <stdarg.h>
 #include <time.h>
-#include <time.h>
 
 FILE *log_file;
-
-    time_t now;
-    time(&now);
-    char buf[sizeof "2025-08-08 08:08:08"];
-    strftime(buf, sizeof buf, "%Y-%m-%d %H:%M:%S", localtime(&now));
-    fprintf(log_file, "[%s] ", buf);
 
 void log_message(const char *format, ...) {
     time_t now;
@@ -31,7 +24,6 @@ void log_message(const char *format, ...) {
     va_start(args, format);
     vfprintf(log_file, format, args);
     va_end(args);
-    fprintf(log_file, "\n");
     fflush(log_file);
 }
 
@@ -67,7 +59,7 @@ void *producer (void * arg) {
 	while(1) {
 		pthread_mutex_lock(&mutex);   // acquire the lock
 		while (count == buff_size) {   // check if the buffer is full
-			log_message("Producer %u is waiting because buffer is full.", id);
+			log_message("Producer %u is waiting because the buffer is full.", id);
 		    pthread_cond_wait(&empty, &mutex);
 		}
 
@@ -81,7 +73,7 @@ void *producer (void * arg) {
         pthread_mutex_unlock(&mutex); // release the lock
 		usleep(500000); // Sleep for 500ms
 	}
-			log_message("Consumer %u is waiting because buffer is empty.", id);
+}
 
 void *consumer (void * arg) {
 	uint32_t id = *((uint32_t *) arg);
@@ -89,33 +81,29 @@ void *consumer (void * arg) {
 		pthread_mutex_lock(&mutex);   // acquire the lock
 
 		while (count == 0) {           // check if the buffer is empty
-			log_message("Consumer %u is waiting because buffer is empty.", id);
-
+			log_message("Consumer %u is waiting because the buffer is empty.", id);
 			pthread_cond_wait(&full, &mutex); // wait for the buffer to be filled
 		}
 
 		head(id);                       // consume (we don't care about the value)!
-	const char log_name[] = "log.txt";
-	log_file = fopen(log_name, "w");
 		pthread_cond_signal(&empty); // signal that the buffer is empty
-        printf("Error opening log file %s\n", log_name);
+        pthread_mutex_unlock(&mutex); // release the lock
 		usleep(500000); // Sleep for 500ms
 	}
 }
 
 int main(int argc, char * argv[]) {
 	const char log_name[] = "log.txt";
-    printf("Saving output into %s", log_name);
+    printf("Saving output into %s \n", log_name);
 	log_file = fopen(log_name, "w");
     if (log_file == NULL) {
-        printf("Error opening log file %s\n", log_name);
+        fprintf(stderr, "Error opening log file. \n");
         exit(1);
     }
 	if (argc < 4) {
-		printf("Usage: ./producer_consumer <buffer_size> <#_of_producers> <#_of_consumers>\n");
+		printf("Usage: ./producer_consumer <buffer_size> <#_of_producers> <#_of_consumers> \n");
 		exit(1);
 	}
-    printf("Saving output into %s", log_name);
 
 	srand(999);
 
